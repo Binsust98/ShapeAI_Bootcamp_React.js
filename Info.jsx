31a9155feb7d488023376409cec29ebc866e5bea
Binsu Susan Thomas <74703301+Binsust98@users.noreply.github.com>
2021-06-25 22:57:22 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JAVASCRIPT AND REACT.JS</h1>
+      <p>
+        A basic web dev React js Bootcamp.Here we bring our another free 7 days
+        live BOOTCAMP.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Info;
